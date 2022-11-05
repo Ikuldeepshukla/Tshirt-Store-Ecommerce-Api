f03965e5804f8106343963edb9092b2bf9c5369e
Kuldeep <kuldeepshukla09496@gmail.com>
2022-11-06 02:23:53 +0530

    added utility WhereClause to get products with search, pagination and filter

--- routes/product.js
+++ routes/product.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.route("/addproduct").get(addProduct);
 
+
+
 module.exports = router;
---
+++ utils/whereClause.js
@@ -0,0 +1,55 @@
+// base - product.find();
+
+// bigQ - //search=coder&page=2&category=shortsleeves&rating[gte]=4&limit=5
+
+class WhereClause {
+  constructor(base, bigQ) {
+    this.base = base;
+    this.bigQ = bigQ;
+  }
+
+  search() {
+    const searchWord = this.bigQ.search
+      ? {
+          name: {
+            $regex: this.bigQ.search,
+            $option: "i",
+          },
+        }
+      : {};
+
+    this.base = this.base.find({ ...searchWord });
+    return this;
+  }
+
+  filter() {
+    const copyQ = { ...this.bigQ };
+    delete copyQ["search"];
+    delete copyQ["limit"];
+    delete copyQ["page"];
+
+    // convert bigQ into a string
+    let stringOfCopyQ = JSON.stringify(copyQ);
+
+    stringOfCopyQ = stringOfCopyQ.replace(
+      /\b(gte|lte|gt|lt)\b/g,
+      (m) => `$${m}`
+    );
+
+    const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+    this.base = this.base.find(jsonOfCopyQ);
+  }
+
+  pager(resultperpage) {
+    let currentPage = 1;
+    if (this.bigQ.page) {
+      currentPage = this.bigQ.page;
+    }
+    const skipVal = resultperpage * (currentPage - 1);
+    this.base = this.base.limit(resultperpage).skip(skipVal);
+    return this;
+  }
+}
+
+module.exports = WhereClause;
