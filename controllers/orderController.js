db369e9095ddd0952249c3887a75efebdfba90de
Kuldeep <kuldeepshukla09496@gmail.com>
2022-11-16 12:42:45 +0530

    added route and controller to get one order details

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require("../models/order");
 const Product = require("../models/product");
 
 const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
 
 exports.createOrder = BigPromise(async (req, res, next) => {
   const {
@@ -22,6 +23,19 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     totalAmount,
     user: req.userId,
   });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getOneOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new CustomError("Please check the order id", 401));
+  }
 
   res.status(200).json({
     success: true,
