e5505ddc0dbd1c7972fd747d67da2300b00d8833
Kuldeep <kuldeepshukla09496@gmail.com>
2022-11-16 12:23:29 +0530

    added router to create order

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,6 @@
 const BigPromise = require("../middlewares/bigPromise");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const nanoid = require("nanoid");
+const { v4: uuidv4 } = require("uuid");
 
 exports.sendStripeKey = BigPromise(async (req, res, next) => {
   res.status(200).json({
@@ -41,7 +41,7 @@ exports.captureRazorpayPayment = BigPromise(async (req, res, next) => {
   const options = {
     amount: req.body.amount,
     currency: "INR",
-    receipt: await nanoid(10),
+    receipt: await uuidv4(),
   };
 
   const myOrder = await instance.orders.create(options);
